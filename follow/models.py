b0f55f66b570afe5e6cf90e7085248725074c1cd
Carlo Gomez <carlo.gomez101@gmail.com>
2018-10-14 12:48:27 +0200

    Database works

--- follow/models.py
+++ follow/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Donor(models.Model):
+	donor_id = models.AutoField(primary_key=True)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	balance = models.DecimalField(decimal_places=2,max_digits=10)
+
+	def increase_balance(self, value):
+		self.balance += value
+
+	def decrease_balance(self, value):
+		self.balance -= value
+
+	def donate(self, r, val):
+		self.decrease_balance(val)
+		r.increase_balance(val)
+		dono = Donation.objects.create(donor=self,recipient=r,value=val)
+		dono.save()
+
+class Recipient(models.Model):
+	recipient_id = models.AutoField(primary_key=True)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	balance = models.DecimalField(decimal_places=2,max_digits=10)
+
+	def increase_balance(self, value):
+		self.balance += value
+
+	def decrease_balance(self, value):
+		self.balance -= value
+
+class Donation(models.Model):
+	donor = models.ForeignKey(Donor, on_delete=models.CASCADE)
+	recipient = models.ForeignKey(Recipient, on_delete=models.CASCADE)
+	value = models.DecimalField(decimal_places=2,max_digits=10)
